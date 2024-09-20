c90732ad047240b757db3e39294c482c6efd4274
pk8101 <pradeepvirat913@gmail.com>
2024-09-21 02:35:13 +0530

    added navbar component and changed all routes to navbar component and changed all the styling for all pages and navbar component also..

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import './App.css';
-import { SignIn,SignUp } from './pages/Main';
-import ForgetPassword  from './pages/signIn/ForgetPassword';
-import CreateNewPassword from './pages/signIn/CreateNewPassword';
-import { BrowserRouter as Router, Routes, Route,Link} from 'react-router-dom';
 import React from 'react';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div>
-    <Router>
-
-      <Link to="/">HOME</Link>
-      <Link to="/signin">Signin</Link>
-      <Link to="/signup">Signup</Link>
-      <Routes>
-        <Route path='/signin' element={<SignIn/>} exact/>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/forgetpassword' element={<ForgetPassword/>}/>
-        <Route path='/resetpassword' element={<CreateNewPassword/>}/>
-      </Routes>
-    </Router>
-    
+     <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './nav.css'
+import { BrowserRouter as Router, Routes, Route,Link} from 'react-router-dom';
+import { SignIn,SignUp } from '../pages/Main';
+import ForgetPassword  from '../pages/signIn/forgetpassword/ForgetPassword';
+import CreateNewPassword from '../pages/signIn/resetpassword/CreateNewPassword';
+function Navbar() {
+  return (
+    
+    <Router>
+        <div className='navbar'>
+            <div className='brand'>
+                <Link to="/">Tracker</Link>
+            </div>
+            <div className='links'>
+                <Link to="/signin">Signin</Link>
+                <Link to="/signup">Signup</Link>
+            </div>
+        </div>
+
+        <Routes>
+            <Route path='/signin' element={<SignIn/>} exact/>
+            <Route path='/signup' element={<SignUp/>}/>
+            <Route path='/forgetpassword' element={<ForgetPassword/>}/>
+            <Route path='/resetpassword' element={<CreateNewPassword/>}/>
+        </Routes>
+    </Router>
+  )
+}
+
+export default Navbar
